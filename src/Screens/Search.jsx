9e539699ca1c9e876276f882977bd1e03b70283d
ACuteWoof <sutharshanvithushan@gmail.com>
2022-06-24 20:53:45 +0530

    links

--- src/Screens/Search.jsx
+++ src/Screens/Search.jsx
@@ -32,7 +32,7 @@ function SearchPage() {
 
   const searchYtMusic = async function (q) {
     const res = await fetch(
-      `https://cors-anywhere.7ih.repl.co/https://yt-dlapi.acutewoof.repl.co/api/${searchMode}/search?q=${q}`,
+      `https://yt-dlapi.acutewoof.repl.co/api/${searchMode}/search?q=${q}`,
       {
         method: "GET",
         headers: {
