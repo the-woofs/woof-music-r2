42d113801001bfcd87a13589a29be03109c9e817
ACuteWoof <sutharshanvithushan@gmail.com>
2022-08-21 21:40:53 +0530

    sync online

--- src/Components/Footer/Right.jsx
+++ src/Components/Footer/Right.jsx
@@ -5,11 +5,15 @@ import VolumeMuteIcon from "@mui/icons-material/VolumeMute";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 import { Slider } from "antd";
+import Sync from "../Sync";
 
 function Right() {
   const [volume, setVolume] = store.useState("volume");
   return (
     <div className="right-component">
+      <div className="sync-status">
+        <Sync />
+      </div>
       <div className="volume">
         {volume === 0 && <VolumeMuteIcon />}
         {volume >= 5 && <VolumeUpIcon />}
---
+++ src/Components/Sync.jsx
@@ -0,0 +1,71 @@
+import { CheckCircleFilled, CloseCircleOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { collection, doc, getFirestore, updateDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSGprUw_eQ-0sEVCLctStEmfunuP5upZU",
+  authDomain: "woof-music.firebaseapp.com",
+  projectId: "woof-music",
+  storageBucket: "woof-music.appspot.com",
+  messagingSenderId: "909712073161",
+  appId: "1:909712073161:web:3a78a1f17cea24c52a8818",
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth();
+
+function Sync(props) {
+  const { hidden } = props;
+  const [user] = useAuthState(auth);
+  const [syncing, setSyncing] = useState(false);
+  const [syncError, setSyncError] = useState(null);
+  const [synced, setSynced] = useState(false);
+
+  useEffect(() => {
+    if (user && auth.currentUser) {
+      setSynced(false);
+      setSyncing(true);
+      setSyncError(false);
+      const userRef = doc(collection(db, "u"), auth.currentUser.uid);
+      updateDoc(userRef, {
+        localStorage: JSON.stringify(localStorage),
+      })
+        .then(() => {
+          console.log("User data updated successfully!");
+          setSynced(true);
+          setSyncing(false);
+          setSyncError(false);
+        })
+        .catch((error) => {
+          console.log("Error updating user data!");
+          setSynced(false);
+          setSyncing(false);
+          setSyncError(true);
+        });
+    } else {
+      setSynced(false);
+      setSyncing(false);
+      setSyncError(true);
+    }
+    // eslint-disable-next-line
+  }, [localStorage, user]);
+
+  return (
+    <>
+      {!hidden && (
+        <>
+          {synced && <CheckCircleFilled />} {syncing && <Spin size="small" />}
+          {syncError && <CloseCircleOutlined />}
+        </>
+      )}
+    </>
+  );
+}
+
+export default Sync;
